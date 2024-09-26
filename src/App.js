f6be1106a50ef4c0338e4e15df9d46f222fddc68
shubham kumari <kumarishubham273@gmail.com>
2024-09-26 12:33:30 +0530

    props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import { CORE_CONCEPTS } from './data.js'
+
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+function Header() {
+  const description = reactDescriptions[genRandomInt(2)]
+  return (
+    <header className="text-white flex flex-col items-center justify-center">
+      <img src="https://scientyficworld.org/wp-content/uploads/2022/08/React-js.jpg" alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {description} React concepts you will need for almost any app you are going to build!
+      </p>
+    </header>
+  );
+}
+
+function CoreConcept({image, title, description}) {
+  return <li className="flex flex-col justify-center items-center">
+    <img src={image} alt={title} />
+    <h3>{title}</h3>
+    <p>{description}</p>
+  </li>
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="text-white flex flex-col items-center justify-center">
+      <Header />
+      <main>
+        <section className="p-1 rounded-md bg-indigo-950 shadow-sm">
+          <h2>core Concepts</h2>
+          <ul className="flex">
+            <CoreConcept {...CORE_CONCEPTS[0]}/>
+            <CoreConcept {...CORE_CONCEPTS[1]}/>
+            <CoreConcept {...CORE_CONCEPTS[2]}/>
+            <CoreConcept {...CORE_CONCEPTS[3]}/>
+            
+          </ul>
+        </section>
+      </main>
     </div>
   );
 }
